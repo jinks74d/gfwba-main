f32e702290e7802550d301dc462ce335d7b95442
Raymond Jenkins <raymond@farsidedev.com>
2023-11-20 08:54:57 -0600

    Metadata Test on Directory

--- app/about/page.js
+++ app/about/page.js
@@ -4,7 +4,6 @@ export default function About() {
   return (
     <main>
       <InnerHero />
-      
     </main>
   );
 }
--- app/directory/page.js
+++ app/directory/page.js
@@ -1,10 +1,47 @@
 import { InnerHero } from "@/devlink";
 
+// *** METADATA
+export const metadata = {
+  title: "GFWBA Member Directory",
+  description: "A searchable directory of GFWBA members",
+  openGraph: {
+    title: "GFWBA Member Directory",
+    description: "A searchable directory of GFWBA members",
+    type: "website",
+    images: [
+      {
+        // *** TODO: Replace with actual image
+        url: "https://gfwba.com/images/hero.jpg",
+        width: 1200,
+        height: 630,
+        alt: "GFWBA Member Directory",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@gfwba",
+    site: "@gfwba",
+    cardType: "summary_large_image",
+  },
+  // *** create schema.org structured data here
+  // *** https://schema.devlink.design or https://schema.org/docs/full.html (for reference)
+  schema: {
+    "@context": "http://schema.org",
+    "@type": "WebSite",
+    name: "GFWBA Member Directory",
+    url: "https://gfwba.com/directory",
+    description: "A searchable directory of GFWBA members",
+  },
+  favicon: {
+    src: "/favicon.ico",
+    type: "image/x-icon",
+  },
+};
+
 export default function Directory() {
   return (
     <main>
-      <InnerHero /> 
-           
+      <InnerHero />
     </main>
   );
 }
