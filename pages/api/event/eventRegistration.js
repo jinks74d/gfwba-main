a37ecec53b44a466fdb0c1657f72eb0f452a4f23
David Newman <davidwnewman91@gmail.com>
2023-12-13 17:03:09 -0600

    progress toward event registration

---
+++ pages/api/event/eventRegistration.js
@@ -0,0 +1,67 @@
+import axios from 'axios';
+import { Buffer } from 'buffer';
+
+export default function handler(req, res) {
+    const { WILD_API, WILD_CLIENT_ID, WILD_CLIENT_SECRET } = process.env
+    let accessToken = '';
+    console.log('line 9: ', req.query)
+
+    const getToken = async () => {
+        try {
+            const apiKey = 'YOUR_API_KEY';
+            const authHeader = `Basic ${Buffer.from(`APIKEY:${WILD_API}`).toString('base64')}`;
+
+            const response = await axios.post(
+                'https://oauth.wildapricot.org/auth/token',
+                'grant_type=client_credentials&scope=auto',
+                {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        Authorization: authHeader,
+                    },
+                }
+            );
+
+            accessToken = response.data.access_token;
+            // Do something with the access token
+            if (response.data.access_token) {
+                createRegistration(accessToken)
+            }
+        } catch (error) {
+            // Handle any errors
+            console.error('Error retrieving access token:', error);
+        }
+    };
+
+    const createRegistration = (token) => {
+        const { registrationInfo, id } = req.body;
+        console.log('start', accessToken == token);
+        console.log('line 39: ', req.query.id);
+        console.log('line 40: ', req.body);
+        axios
+            .put(`https://api.wildapricot.org/v2.2/accounts/191317/events/${id}/registrations`, registrationInfo, {
+                headers: {
+                    'User-Agent': 'MySampleApplication/0.1',
+                    Accept: 'application/json',
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            })
+            .then((response) => {
+                const event = response.data;
+                // Do something with the event
+                res.status(200).json(event);
+            })
+            .catch((error) => {
+                // Handle any errors
+                console.error('Error retrieving event:', error);
+            });
+    };
+
+    if (req.method === 'POST') {
+        // Process a POST request
+        getToken()
+    } else {
+        // Handle any other HTTP method
+        console.log(req.method)
+    }
+}
