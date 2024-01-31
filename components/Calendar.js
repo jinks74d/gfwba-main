386a68e8cc618388615f09049e58e44d2c924283
Raymond Jenkins <raymond@farsidedev.com>
2024-01-31 10:32:48 -0600

    Webflow Update

--- components/Calendar.js
+++ components/Calendar.js
@@ -12,7 +12,7 @@ import Link from "next/link";
 export function CalendarComponent({ events }) {
   console.log(events);
   return (
-    <div className="h-[100vh]">
+    <div className="h-full">
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         headerToolbar={{
