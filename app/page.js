751d950cd54d1c48b8538b1631ab8fbbcc2cd522
Raymond Jenkins <raymond@farsidedev.com>
2024-03-21 13:05:02 -0500

    Refactor lightbox functionality in AdSidebar component

--- app/page.js
+++ app/page.js
@@ -7,41 +7,41 @@ import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 const { metadataBase } = publicRuntimeConfig;
 
-export const metadata = {
-  title:
-    "Greater Fort Worth Builders Association - Leading Homebuilding Industry Advocacy",
-  description:
-    "Join GFWBA, the premier non-profit association for homebuilding professionals in Fort Worth. Advocacy, education, networking, and member services since 1945.",
-  authors: [
-    {
-      name: "Farside Web Development",
-      url: "https://farsidedev.com",
-    },
-    {
-      name: "Greater Fort Worth Builders Association",
-      url: "https://gfwbatx.com",
-    },
-  ],
-  openGraph: {
-    title:
-      "Empowering Fort Worth's Homebuilding Industry - Greater Fort Worth Builders Association",
-    description:
-      "Discover how the Greater Fort Worth Builders Association champions the homebuilding industry through advocacy, education, and networking. Since 1945, connecting professionals for growth and community development.",
-    url: `${metadataBase}`,
-    siteName: "Greater Fort Worth Builders Association",
-    locale: "en_US",
-    type: "website",
-    images: [
-      {
-        url: `${metadataBase}/public/images/gfwba-logo@2x.png`,
-        alt: "GFWBA Logo",
-      },
-    ],
-  },
-};
+// export const metadata = {
+//   title:
+//     "Greater Fort Worth Builders Association - Leading Homebuilding Industry Advocacy",
+//   description:
+//     "Join GFWBA, the premier non-profit association for homebuilding professionals in Fort Worth. Advocacy, education, networking, and member services since 1945.",
+//   authors: [
+//     {
+//       name: "Farside Web Development",
+//       url: "https://farsidedev.com",
+//     },
+//     {
+//       name: "Greater Fort Worth Builders Association",
+//       url: "https://gfwbatx.com",
+//     },
+//   ],
+//   openGraph: {
+//     title:
+//       "Empowering Fort Worth's Homebuilding Industry - Greater Fort Worth Builders Association",
+//     description:
+//       "Discover how the Greater Fort Worth Builders Association champions the homebuilding industry through advocacy, education, and networking. Since 1945, connecting professionals for growth and community development.",
+//     url: `${metadataBase}`,
+//     siteName: "Greater Fort Worth Builders Association",
+//     locale: "en_US",
+//     type: "website",
+//     images: [
+//       {
+//         url: `${metadataBase}/public/images/gfwba-logo@2x.png`,
+//         alt: "GFWBA Logo",
+//       },
+//     ],
+//   },
+// };
 
 export default function Home() {
-  const { url } = metadata.openGraph;
+  // const { url } = metadata.openGraph;
 
   // Schema.org JSON-LD for SEO
   // Define the Organization schema
@@ -129,7 +129,7 @@ export default function Home() {
         <script type="application/ld+json">
           {JSON.stringify(breadcrumbSchema)}
         </script>
-        <link rel="canonical" href={url} />
+        {/* <link rel="canonical" href={url} /> */}
       </head>
       <main>
         <HomeHero
--- components/AdSidebar.js
+++ components/AdSidebar.js
@@ -4,12 +4,20 @@ import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 
 export default function AdSidebar() {
-  const [open, setOpen] = useState(false);
+  // Use an object to manage open state of multiple lightboxes
+  const [open, setOpen] = useState({});
+
+  // Function to handle opening a specific lightbox
+  const handleOpen = (id) => setOpen({ ...open, [id]: true });
+
+  // Function to handle closing a specific lightbox
+  const handleClose = (id) => setOpen({ ...open, [id]: false });
 
   return (
     <>
-      <div className="mb-6 border border-gray-400">
-        <button type="button" onClick={() => setOpen(true)}>
+      {/* Each lightbox section now has a unique identifier */}
+      <div className="mb-8 border border-gray-400">
+        <button type="button" onClick={() => handleOpen("lightbox1")}>
           <Image
             src="/images/ads/2023Contracts_w-qr1pgx700-min.jpg"
             alt="2023 Contracts"
@@ -19,45 +27,64 @@ export default function AdSidebar() {
         </button>
 
         <Lightbox
-          open={open}
-          close={() => setOpen(false)}
+          open={open["lightbox1"]}
+          close={() => handleClose("lightbox1")}
           slides={[{ src: "/images/ads/2023Contracts_w-qr1pgx700-min.jpg" }]}
         />
       </div>
 
-      <div className="mb-6 border border-gray-400">
-        <button type="button" onClick={() => setOpen(true)}>
+      <div className="mb-8 border border-gray-400">
+        <button type="button" onClick={() => handleOpen("lightbox2")}>
           <Image
             src="/images/ads/2024ModelSafety_HalfPg.jpg"
-            alt="2024 Safety Model"
+            alt="2023 Contracts"
             width={300}
             height={600}
           />
         </button>
 
         <Lightbox
-          open={open}
-          close={() => setOpen(false)}
+          open={open["lightbox2"]}
+          close={() => handleClose("lightbox2")}
           slides={[{ src: "/images/ads/2024ModelSafety_HalfPg.jpg" }]}
         />
       </div>
 
-      <div className="mb-6 border border-gray-400">
-        <button type="button" onClick={() => setOpen(true)}>
+      <div className="mb-8 border border-gray-400">
+        <button type="button" onClick={() => handleOpen("lightbox3")}>
           <Image
             src="/images/ads/TABInsuranceProgram-w-QRx720-min.png"
-            alt="2024 Safety Model"
+            alt="2023 Contracts"
             width={300}
             height={600}
           />
         </button>
 
         <Lightbox
-          open={open}
-          close={() => setOpen(false)}
-          slides={[{ src: "TABInsuranceProgram-w-QRx720-min.png" }]}
+          open={open["lightbox3"]}
+          close={() => handleClose("lightbox3")}
+          slides={[{ src: "/images/ads/TABInsuranceProgram-w-QRx720-min.png" }]}
         />
       </div>
+
+      <div className="mb-8 border border-gray-400">
+        <button type="button" onClick={() => handleOpen("lightbox4")}>
+          <Image
+            src="/images/ads/TAB-Q4'22-Flyer-min.jpg"
+            alt="2023 Contracts"
+            width={300}
+            height={600}
+          />
+        </button>
+
+        <Lightbox
+          open={open["lightbox4"]}
+          close={() => handleClose("lightbox4")}
+          slides={[{ src: "/images/ads/TAB-Q4'22-Flyer-min.jpg" }]}
+        />
+      </div>
+
+      {/* Repeat for each lightbox, ensuring to change the identifier */}
     </>
   );
 }
