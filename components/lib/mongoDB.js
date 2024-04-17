bd1da089ba1b7ce91550244e8a72d2be17efcafa
usmancherrybyte <muhammad.usman@thecherrybyte.com>
2024-04-17 22:53:47 +0500

    updates url

--- components/lib/mongoDB.js
+++ components/lib/mongoDB.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     if (mongoose.connection.readyState === 0) {
-      await mongoose.connect(process.env.MONGODB_URI);
+      await mongoose.connect(
+        "mongodb+srv://umth5898:RjORsgelUCYj9f4C@cluster0.dxswetf.mongodb.net/wild-Apricot-test"
+      );
       console.log("db connected");
     }
   } catch (error) {
--- pages/api/cron/update.js
+++ pages/api/cron/update.js
@@ -1,36 +1,49 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
       const { contactID, image } = req.body;
-      
+
       // Check if image data and contactID are provided
       if (!contactID || !image) {
-        return res.status(400).json({ success: false, message: "Missing required fields" });
+        return res
+          .status(400)
+          .json({ success: false, message: "Missing required fields" });
       }
 
-      const imageData = image.split(';base64,').pop(); // Extract base64 data
-      const imagePath = path.join(process.cwd(), 'public/contacts-image', `image_${contactID}.jpg`);
-      
+      const imageData = image.split(";base64,").pop(); // Extract base64 data
+      const imagePath = path.join(
+        process.cwd(),
+        "public/contacts-image",
+        `image_${contactID}.jpg`
+      );
+
       // Check if directory exists, if not create it
       if (!fs.existsSync(path.dirname(imagePath))) {
         fs.mkdirSync(path.dirname(imagePath), { recursive: true });
       }
 
-      fs.writeFile(imagePath, imageData, 'base64', function (err) {
+      fs.writeFile(imagePath, imageData, "base64", function (err) {
         if (err) {
-          console.error('Error:', err);
-          return res.status(500).json({ success: false, message: "Image uploading to local machine failed" });
+          console.error("Error:", err);
+          return res
+            .status(500)
+            .json({
+              success: false,
+              message: "Image uploading to local machine failed",
+            });
         } else {
-          console.log('Image saved successfully at:', imagePath);
-          return res.status(200).json({ success: true, message: "Image uploaded successfully" });
+          console.log("Image saved successfully at:", imagePath);
+          return res
+            .status(200)
+            .json({ success: true, message: "Image uploaded successfully" });
         }
       });
     } catch (error) {
       console.log("Error:", error);
-      res.status(500).json({ success: false, message: "Internal server error" });
+      res.status(500).json({ success: false, message: error });
     }
   } else {
     res.status(405).json({ success: false, message: "Method not allowed" });
