91420db516937d0f3913bb6020b5445134263b7d
usmancherrybyte <muhammad.usman@thecherrybyte.com>
2024-04-18 23:02:22 +0500

    change cron logic

--- pages/api/cron/hit.js
+++ pages/api/cron/hit.js
@@ -56,6 +56,98 @@ const getToken = async () => {
   }
 };
 
+// export default async function handler(req, res) {
+//   if (req.method === "GET") {
+//     const token = await getToken();
+//     console.log("token", token);
+//     console.log("contact", ContactImage);
+
+//     try {
+//       const response = await axios.get(
+//         `https://api.wildapricot.com/v2.1/accounts/191317/contacts?$async=false&%24filter=Status%20eq%20'Active'`,
+//         {
+//           headers: {
+//             "User-Agent": "MySampleApplication/0.1",
+//             Accept: "application/json",
+//             Authorization: `Bearer ${token}`,
+//           },
+//         }
+//       );
+
+//       const parentContacts = response.data.Contacts;
+//       const childContacts = await ContactImage.find().sort({ updatedAt: 1 });
+//       const newContacts = [];
+//       const alreadyExists = [];
+
+//       for (const parentContact of parentContacts) {
+//         const existingChildContact = childContacts.find(
+//           (childContact) => childContact.wildapricotUserId === parentContact.Id
+//         );
+
+//         if (!existingChildContact) {
+//           newContacts.push(parentContact);
+//         } else {
+//           alreadyExists.push(parentContact);
+//         }
+//       }
+
+//       const allContacts = [...newContacts, ...alreadyExists];
+
+//       for (let i = 0; i < allContacts.length; i++) {
+//         const contact = allContacts[i];
+
+//         const existingImage = await ContactImage.findOne({
+//           wildapricotUserId: contact.Id,
+//         });
+
+//         if (contact.FieldValues[49]?.Value?.Url) {
+//           const imageName = contact.FieldValues[49].Value.Id;
+//           const imageResponse = await axios.get(
+//             contact.FieldValues[49].Value.Url,
+//             {
+//               responseType: "arraybuffer",
+//               headers: {
+//                 "User-Agent": "MySampleApplication/0.1",
+//                 Accept: "application/json",
+//                 Authorization: `Bearer ${token}`,
+//               },
+//             }
+//           );
+
+//           const blob = await put(
+//             `${contact.Id}.jpg`,
+//             Buffer.from(imageResponse.data, "binary"), // Convert arraybuffer to Buffer
+//             { access: "public" }
+//           );
+//           if (!existingImage) {
+//             const newImage = new ContactImage({
+//               wildapricotUserId: contact.Id,
+//               wildapricotUrl: contact.FieldValues[49].Value.Url,
+//               vercelBolbUrl: blob.url,
+//               updatedAt: Date.now(),
+//             });
+//             await newImage.save();
+//           } else {
+//             existingImage.updatedAt = Date.now();
+//             await existingImage.save();
+//           }
+//         } else {
+//           console.log("Field data doesn't have a value");
+//         }
+//       }
+
+//       res.status(200).json({ success: true });
+//     } catch (error) {
+//       console.log("error", error);
+//       res
+//         .status(500)
+//         .json({ success: false, message: "Internal server error" });
+//     }
+//   } else {
+//     res.status(405).json({ success: false, message: "Method not allowed" });
+//   }
+// }
+
 export default async function handler(req, res) {
   if (req.method === "GET") {
     const token = await getToken();
@@ -74,66 +166,63 @@ export default async function handler(req, res) {
         }
       );
 
-      const parentContacts = response.data.Contacts;
-      const childContacts = await ContactImage.find().sort({ updatedAt: 1 });
-      const newContacts = [];
-      const alreadyExists = [];
+      const contactsData = response.data.Contacts;
 
-      for (const parentContact of parentContacts) {
-        const existingChildContact = childContacts.find(
-          (childContact) => childContact.wildapricotUserId === parentContact.Id
-        );
-
-        if (!existingChildContact) {
-          newContacts.push(parentContact);
-        } else {
-          alreadyExists.push(parentContact);
-        }
-      }
-
-      const allContacts = [...newContacts, ...alreadyExists];
-
-      for (let i = 0; i < allContacts.length; i++) {
-        const contact = allContacts[i];
-
+      for (let i = 0; i < contactsData.length; i++) {
         const existingImage = await ContactImage.findOne({
-          wildapricotUserId: contact.Id,
+          wildapricotUserId: contactsData[i].Id,
         });
-
-        if (contact.FieldValues[49]?.Value?.Url) {
-          const imageName = contact.FieldValues[49].Value.Id;
-          const imageResponse = await axios.get(
-            contact.FieldValues[49].Value.Url,
-            {
-              responseType: "arraybuffer",
-              headers: {
-                "User-Agent": "MySampleApplication/0.1",
-                Accept: "application/json",
-                Authorization: `Bearer ${token}`,
-              },
-            }
-          );
-
-          const blob = await put(
-            `${contact.Id}.jpg`,
-            Buffer.from(imageResponse.data, "binary"), // Convert arraybuffer to Buffer
-            { access: "public" }
-          );
+        if (contactsData[i].FieldValues[49].Value.Url) {
+          const imageName = contactsData[i].FieldValues[49].Value.Id;
           if (!existingImage) {
+            const imageResponse = await axios.get(
+              contactsData[i].FieldValues[49].Value.Url,
+
+              {
+                responseType: "arraybuffer",
+                headers: {
+                  "User-Agent": "MySampleApplication/0.1",
+                  Accept: "application/json",
+                  Authorization: `Bearer ${token}`,
+                },
+              }
+            );
+
+            const blob = await put(
+              `${contactsData.Id}.jpg`,
+              Buffer.from(imageResponse.data, "binary"), // Convert arraybuffer to Buffer
+              { access: "public" }
+            );
             const newImage = new ContactImage({
-              wildapricotUserId: contact.Id,
-              wildapricotUrl: contact.FieldValues[49].Value.Url,
+              wildapricotUserId: contactsData[i].Id,
+              wildapricotUrl: contactsData[i].FieldValues[49].Value.Url,
               vercelBolbUrl: blob.url,
               updatedAt: Date.now(),
             });
             await newImage.save();
-          } else {
-            existingImage.updatedAt = Date.now();
+          } else if (!existingImage.vercelBolbUrl) {
+            const imageResponse = await axios.get(
+              contactsData[i].FieldValues[49].Value.Url,
+
+              {
+                responseType: "arraybuffer",
+                headers: {
+                  "User-Agent": "MySampleApplication/0.1",
+                  Accept: "application/json",
+                  Authorization: `Bearer ${token}`,
+                },
+              }
+            );
+
+            const blob = await put(
+              `${contactsData.Id}.jpg`,
+              Buffer.from(imageResponse.data, "binary"), // Convert arraybuffer to Buffer
+              { access: "public" }
+            );
+            existingImage.vercelBolbUrl = blob.url;
             await existingImage.save();
           }
-        } else {
-          console.log("Field data doesn't have a value");
-        }
+        } else console.log("Field data dont have value");
       }
 
       res.status(200).json({ success: true });
