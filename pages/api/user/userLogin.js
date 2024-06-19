e9487c83863f2486c6a8685fa1d9acfba37f2ae3
David Newman <davidwnewman91@gmail.com>
2024-06-19 12:03:54 -0500

    updated login api call

--- pages/api/user/userLogin.js
+++ pages/api/user/userLogin.js
@@ -6,17 +6,17 @@ export default function handler(req, res) {
     const { WILD_API, WILD_CLIENT_ID, WILD_CLIENT_SECRET, USER_EMAIL, USER_PASSWORD } = process.env
     let accessToken = '';
     let accountId = '';
-    console.log(req.body)
+    // console.log(req.body)
     const { username, password } = req.body
 
     const getToken = async () => {
         try {
             const authHeader = `Basic ${Buffer.from(`${WILD_CLIENT_ID}:${WILD_CLIENT_SECRET}`).toString('base64')}`;
-            console.log(`grant_type=password&username=${username}&password=${password}&scope=auto`);
+            // console.log(`grant_type=password&username=${username}&password=${password}&scope=auto`);
 
             const response = await axios.post(
                 'https://oauth.wildapricot.org/auth/token',
-                `grant_type=password&username=${username}&password=${password}&scope=auto`,
+                `grant_type=password&username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}&scope=auto`,
                 {
                     headers: {
                         'Content-Type': 'application/x-www-form-urlencoded',
@@ -33,7 +33,7 @@ export default function handler(req, res) {
         } catch (error) {
             // Handle any errors
             // console.error('Error retrieving access token:', error);
-            console.log('Error retrieving access token:', error.response.data.error_description);
+            // console.log('Error retrieving access token:', error.response.data.error_description);
             res.status(401).json(error.response.data);
         }
     };
