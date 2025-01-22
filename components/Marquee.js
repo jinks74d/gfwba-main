69608f1f2ce05b3c3289ef219b4989cce30d0591
Raymond Jenkins <raymond@farsidedev.com>
2025-01-22 15:10:34 -0600

    kjh

--- components/Marquee.js
+++ components/Marquee.js
@@ -68,7 +68,6 @@ const Marq = () => {
           <Image
             src={WPLogo}
             alt="Western Pacific Logo"
-            width={100}
             height={99}
             className="image-scroll-item"
           />
